14b2e8a606550f838592b09c30f0a21c3ef1e5dd
Matthew Jia <jiamatt@amazon.com>
2025-06-18 13:04:56 -0700

    feat: update formula to v1.2.0 with multiple todo operations

--- todo-cli.rb
+++ todo-cli.rb
@@ -3,20 +3,35 @@ class TodoCli < Formula
 
   desc "Simple, beautiful command-line todo application for developers"
   homepage "https://github.com/Matthew-Jia/todo-cli"
-  url "https://github.com/Matthew-Jia/todo-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2adfb4cbcdcc678fb88230743a6d59344f80b0f35b50eef7f374cead66b1e784"
+  url "https://github.com/Matthew-Jia/todo-cli/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "41df6d4f5dba777e6d8aefbc6cccf01c05f7e4f58402f4bd0671f1139f4480b2"
   license "MIT"
 
-  depends_on "python@3.9"
+  depends_on "python@3.11"
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
-    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/38/71/3b932c671bbd2428d4c302450e3a2db8fb6c9ee4f8c0e8b33d0f0f8a7c1e/markdown-it-py-3.0.0.tar.gz"
+    sha256 "e3f60a94fa066dc52ec76661e37c851cb232d92f9886b15cb560aaada2df8feb"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/55/59/8bccf4157baf25e4aa5a0bb7fa3ba8600907de105ebc22b0c78cfbf6f565/pygments-2.17.2.tar.gz"
+    sha256 "da46cec9fd2de5be3a8a784f434e4c4ab670b4ff54d605c4c2717e9d49c4c367"
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/11/23/814edf09ec6470d52022b9e95c23c1bef77f0bc451761e1504ebd09606d3/rich-12.6.0.tar.gz"
-    sha256 "ba3a3775974105c221d31141f2c116f4fd65c5ceb0698657a11e9f295ec93fd0"
+    url "https://files.pythonhosted.org/packages/a7/ec/4a7d80728bd429f7c0d4d51245287158a1516315cadbb146012439403a9d/rich-13.7.0.tar.gz"
+    sha256 "5cb5123b5cf9ee70584244246816e9114227e0b98ad9176eede6ad54bf5403fa"
   end
 
   def install
@@ -24,8 +39,20 @@ class TodoCli < Formula
   end
 
   test do
-    # Add a todo and verify it was added
+    # Test basic functionality
     assert_match "Added todo", shell_output("#{bin}/todo a 'Test todo'")
     assert_match "Test todo", shell_output("#{bin}/todo l")
+    
+    # Test shorthand priority
+    assert_match "high", shell_output("#{bin}/todo a 'High priority todo' -p h")
+    
+    # Test new multiple ID functionality
+    system bin/"todo", "a", "Todo 1"
+    system bin/"todo", "a", "Todo 2"
+    assert_match "2 todos as completed", shell_output("#{bin}/todo c 0 1")
+    
+    # Test --all flag functionality
+    system bin/"todo", "a", "Todo 3"
+    assert_match "1 todos as completed", shell_output("#{bin}/todo c --all")
   end
 end
